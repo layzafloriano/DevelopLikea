7abaa9fc2864eb6c033491a2fce20b9b4468da3e
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-07-13 12:24:17 -0300

    user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Pending Confirmation', 'Active']
+  },
+  confirmationCode: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mentor: Boolean,
+  openToOpportunities: Boolean,
+  city: String,
+  specialty: {
+    type: String,
+    enum: ['FrontEnd', 'BackEnd', 'FullStack', 'Estudante', 'Outro'],
+  },
+  bio: String,
+  number: String,
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
