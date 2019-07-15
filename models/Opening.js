29f2ccfde2e441da275897ae96c8b946b09c3b96
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-07-15 10:46:05 -0300

    added opening model, routes and views

---
+++ models/Opening.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const openingSchema = new Schema({
+  title: String,
+  description: String,
+  company: String,
+  salary: String,
+  requirements: String,
+  author: [{ type: Schema.Types.ObjectId, ref: 'Author' }],
+  location: { type: { type: String }, coordinates: [Number] },
+  type: {
+    type: String,
+    enum: ['CLT', 'PJ']
+  },
+  level: {
+    type: String,
+    enum: ['internship', 'junior', 'intermediate', 'senior'],
+  },
+  city: String,
+}, {
+  timestamps: true,
+});
+
+openingSchema.index({ location: '2dsphere' });
+
+const Opening = mongoose.model('Opening', openingSchema);
+
+module.exports = Opening;
